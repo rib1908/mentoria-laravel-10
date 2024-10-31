4832d4e0a45dfeb76c897368a14f8a48a5e72481
rib1908 <rib.js198@gmail.com>
2024-10-31 14:38:24 -0300

    Correção import jquery e blockUI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -335,10 +335,15 @@
 </div>
 
     @yield('scripts')
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
     <script src="/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"></script>
     <script src="/js/color-modes.js"></script>
     <script src="/js/dashboard.js"></script>
+    
+    {{-- BlocUI loading --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
         </body>
 </html>
