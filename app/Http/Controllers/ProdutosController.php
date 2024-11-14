69cf4d131979bdcaa4edc7fc6a62fc6b9c5c2a99
rib1908 <rib.js198@gmail.com>
2024-11-14 16:35:20 -0300

    import Toastr

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FormRequestProduto;
 use App\Models\Componentes;
 use App\Models\Produto;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
@@ -53,17 +54,21 @@ class ProdutosController extends Controller
     {
         if($request->method() == "PUT") {
             
-            // Cria os dados
-            //$data = $request->all();
-            //$componentes = new Componentes();
+            // Atualiza os dados
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            $buscaRegistro = Produto::find($id);
+            $buscaRegistro->update($data);
 
-            //$data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
-            //Produto::create($data);
-
-            //return redirect()->route('produto.index');
+            Toastr::success("Produto gravado com sucesso!");
+            return redirect()->route('produto.index');
         }
 
-        return view('pages.produtos.atualiza');
+        // mostrar os dados
+        $findProduto = Produto::where('id', '=', $id)-> first();
+        return view('pages.produtos.atualiza', compact('findProduto'));
+        
     }
 }
 
